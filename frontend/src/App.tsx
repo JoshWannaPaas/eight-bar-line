df82165a84baea78414ada481fe5901a4aa5a764
JoshWannaPaas <paasjoshua@yahoo.com>
2023-09-28 22:21:29 -0700

    Updated main page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     // <>
@@ -29,8 +29,11 @@ function App() {
     //     Click on the Vite and React logos to learn more
     //   </p>
     // </>
-    <h1>8Bar Line</h1>
-  )
+    <>
+      <h1>8Bar Line</h1>
+      <p>test</p>
+    </>
+  );
 }
 
-export default App
+export default App;
