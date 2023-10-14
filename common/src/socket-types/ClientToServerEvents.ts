b39dabed5e2409c0a6479a9427fad6f89657f1fb
Tom <tommy.seng.heng@gmail.com>
2023-10-13 22:52:58 -0400

    removed redundent labels

--- common/src/socket-types/ClientToServerEvents.ts
+++ common/src/socket-types/ClientToServerEvents.ts
@@ -9,8 +9,6 @@ interface ClientToServerEvents {
   // A dummy endpoint for demo-ing how to use sockets
   ping: (message: string) => void;
 
-  // Room-related endpoints
-
   /** Creates a new room and returns the room's code */
   ["room:create"]: () => RoomCode;
   /** Joins the room with the given room code, if it exists */
@@ -19,8 +17,6 @@ interface ClientToServerEvents {
   ["room:leave"]: () => void;
   /** Broadcasts a message to every other member of the room */
   ["room:send-message"]: (message: string) => void;
-
-  // Ensemble-related endpoints
 
   /** Read the current state of the board */
   ["ensemble:fetch"]: () => EnsembleObject;
