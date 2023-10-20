a8cf7e878a042b9d3f404c2784d2c5073f3b8d76
JoshWannaPaas <paasjoshua@yahoo.com>
2023-10-19 20:38:34 -0700

    feat: added visual note changes to ensemble view

---
+++ frontend/src/views/ensemble/Board.tsx
@@ -0,0 +1,39 @@
+import { Box } from "@mui/material";
+import _ from 'lodash';
+import SingleNote from "./SingleNote";
+
+// consts for calculating board, not including volume bar
+const boardWidth = 32; // 8 bars of 4 quarter notes = 32 note placements
+const boardHeight = 8; // 8 Notes from C -> C
+
+// temp name for music editor area
+const Board: React.FC = () => {
+  return (
+    <Box>
+      {_.range(boardHeight).map((n) => (
+        <SingleRow key={`note-pitch-${n}`} />
+      ))}
+    </Box>
+  );
+};
+export default Board;
+
+const SingleRow: React.FC = () => {
+  return (
+    <Box display="flex">
+      {_.range(boardWidth).map((n) => (
+        <SingleNote key={`note-number-${n}`} />
+      ))}
+    </Box>
+  );
+};
+
+export const VolumeRow: React.FC = () => {
+  return (
+    <Box display="flex">
+      {_.range(boardWidth).map((n) => (
+        <SingleNote key={`note-volume-${n}`} />
+      ))}
+    </Box>
+  );
+};
--- frontend/src/views/ensemble/index.tsx
+++ frontend/src/views/ensemble/index.tsx
@@ -1,8 +1,7 @@
 import { FC } from "react";
-import "./index.css";
 import { Box, Container, Paper, Stack, styled } from "@mui/material";
-import Divider from '@mui/material/Divider'
 import _ from "lodash";
+import Board, {VolumeRow} from './Board';
 
 const EnsembleView: FC = () => {
   return (
@@ -10,7 +9,6 @@ const EnsembleView: FC = () => {
       {/* Top Line for Instrument Settings, Title, and Room Settings */}
       <Stack
         direction="row"
-        divider={<Divider orientation="vertical" flexItem />}
         justifyContent="space-evenly"
         alignItems="center"
         paddingTop="1%"
@@ -31,7 +29,15 @@ const EnsembleView: FC = () => {
       </Stack>
 
       {/* Your play area */}
-      <Container id="playerSelf">
+      <Container
+        sx={{
+          margin: "auto",
+          marginTop: "2%",
+          alignSelf: "center",
+          alignItems: "center",
+          padding: "0px",
+        }}
+      >
         <Board />
         <br />
         <VolumeRow />
@@ -47,7 +53,6 @@ const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
   ...theme.typography.body2,
   paddingLeft: "15px",
-  // textAlign: "center",
   display: "flex",
   flexDirection: "column",
   alignItems: "baseline",
@@ -55,48 +60,3 @@ const Item = styled(Paper)(({ theme }) => ({
   color: theme.palette.text.secondary,
 }));
 
-// consts for calculating board, not including volume bar
-const boardWidth = 32; // 8 bars of 4 quarter notes = 32 note placements
-const boardHeight = 8; // 8 Notes from C -> C
-
-// temp name for music editor area
-const Board: React.FC = () => {
-  return (
-    <Box>
-      {_.range(boardHeight).map((n) => (
-        <SingleRow key={`note-pitch-${n}`} />
-      ))}
-    </Box>
-  );
-};
-
-const SingleRow: React.FC = () => {
-  return (
-    <RowBox>
-      {_.range(boardWidth).map((n) => (
-        <SingleNote key={`note-number-${n}`} />
-      ))}
-    </RowBox>
-  );
-};
-
-const VolumeRow: React.FC = () => {
-  return (
-    <RowBox>
-      {_.range(boardWidth).map((n) => (
-        <SingleNote key={`note-volume-${n}`} />
-      ))}
-    </RowBox>
-  );
-}
-
-const RowBox = styled(Container)({
-  display: "flex"
-});
-
-const SingleNote = styled(Box)({
-  minWidth: 32,
-  minHeight: 32,
-  backgroundColor: "lightblue",
-  margin: "2px",
-});
