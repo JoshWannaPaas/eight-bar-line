78c14a398dbd59376fdb0b26eed3b3409f9e489c
Tom <tommy.seng.heng@gmail.com>
2023-11-20 20:37:09 -0500

    feat(backend): sockets store their room code

--- common/src/socket-types/SocketData.ts
+++ common/src/socket-types/SocketData.ts
@@ -1,7 +1,11 @@
+import { RoomCode } from "./ClientToServerEvents.js";
+
 /**
  * Additional information that is attached to each client
  */
 interface SocketData {
   username: string;
+  roomCode: RoomCode | undefined;
 }
+
 export default SocketData;
