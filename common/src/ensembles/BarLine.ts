62e04f2fcbf510e8466f3896fd30b3ed0ef140e9
Tommy Seng Heng <tommy.seng.heng@gmail.com>
2023-12-13 00:11:32 -0500

    feat(common): docstrings (#39)
    * feat(common): docstrings
    
    * docstrings

--- common/src/ensembles/BarLine.ts
+++ common/src/ensembles/BarLine.ts
@@ -50,68 +50,101 @@ export class BarLine {
     this.state = initialState;
   }
 
+  /**
+   * Returns the author of this BarLine
+   * @returns The author of this BarLine
+   */
   getAuthor() {
     return this.state.author;
   }
 
+  /**
+   * Returns the instrument of this BarLine
+   * @returns The instrument of this BarLine
+   */
   setInstrument(instrument: Instrument) {
     this.state.instrument = instrument;
   }
 
   /**
-   * Returns true if the given `row` and `col` are in range of 
-   * this barline. 
+   * @returns true if the given `row` and `col` are in range of
+   * this barline.
    */
-  static inRange (row: number, col: number) {
+  static inRange(row: number, col: number) {
     if (row < 0 || row > BarLine.ROWS) return false;
     if (col < 0 || col > BarLine.COLS) return false;
     return true;
   }
 
+  /**
+   * Returns the note at the given position. Returns undefined if the
+   * position is out of range.
+   * 
+   * @param row The row of the note
+   * @param col The column of the note
+   * @returns The note at the given position
+   */
   getNote(row: number, col: number): Note | undefined {
     if (!BarLine.inRange(row, col)) return undefined;
     return this.state.notes[row][col];
   }
 
-  setNote(row: number, col: number, note: Note) : void{
+  /**
+   * Sets the note at the given position. Does nothing if the position
+   * is out of range.
+   * 
+   * @param row The row of the note
+   * @param col The column of the note
+   * @param note The note to set
+   */
+  setNote(row: number, col: number, note: Note): void {
     if (!BarLine.inRange(row, col)) return;
     this.state.notes[row][col] = note;
   }
 
-  getNoteType (row: number, col: number): NoteType | undefined {
+  /**
+   * Gets the type of the note at the given position. Returns undefined
+   * if the position is out of range.
+   * 
+   * @param row The row of the note
+   * @param col The column of the note
+   * @returns The type of the note at the given position
+   */
+  getNoteType(row: number, col: number): NoteType | undefined {
     return this.getNote(row, col)?.type;
   }
 
   /**
    * Updates the note at the given position according to the following
-   * rules. If the note becomes a REST and the next note is a SUSTAIN, 
+   * rules. If the note becomes a REST and the next note is a SUSTAIN,
    * we set that note to ATTACK instead.
-   * 
-   * Forward Direction: 
+   *
+   * Forward Direction:
    *   REST -> ATTACK -> SUSTAIN (skipped if prev note is REST)
    * Backward Direction:
    *   REST -> SUSTAIN (skipped if prev note is REST) -> ATTACK
    */
-  toggleNote (row: number, col: number, direction: NoteToggleDirection) {
-    if (!BarLine.inRange(row, col)) return; 
+  toggleNote(row: number, col: number, direction: NoteToggleDirection) {
+    if (!BarLine.inRange(row, col)) return;
     const note = this.getNote(row, col);
     // Behavior for the Forward Direction
     if (direction === NoteToggleDirection.FORWARD) {
-      switch(note.type) {
+      switch (note.type) {
         case NoteType.REST: {
           note.type = NoteType.ATTACK;
           return;
         }
         case NoteType.ATTACK: {
-          const priorNoteType = this.getNoteType(row, col-1);
-          const canBecomeSustain = ALLOWED_SUSTAIN_PRECEDENTS.includes(priorNoteType);
+          const priorNoteType = this.getNoteType(row, col - 1);
+          const canBecomeSustain =
+            ALLOWED_SUSTAIN_PRECEDENTS.includes(priorNoteType);
           note.type = canBecomeSustain ? NoteType.SUSTAIN : NoteType.REST;
           return;
         }
         case NoteType.SUSTAIN: {
           note.type = NoteType.REST;
           // If the next note was a SUSTAIN, it becomes an ATTACK
-          const nextNote = this.getNote(row, col+1);
+          const nextNote = this.getNote(row, col + 1);
           if (nextNote === undefined || nextNote.type !== NoteType.SUSTAIN)
             return;
           nextNote.type = NoteType.ATTACK;
@@ -121,17 +154,18 @@ export class BarLine {
     }
 
     // Behavior for the Backward Direction
-    switch(note.type) {
+    switch (note.type) {
       case NoteType.REST: {
-        const priorNoteType = this.getNoteType(row, col-1);
-        const canBecomeSustain = ALLOWED_SUSTAIN_PRECEDENTS.includes(priorNoteType);
+        const priorNoteType = this.getNoteType(row, col - 1);
+        const canBecomeSustain =
+          ALLOWED_SUSTAIN_PRECEDENTS.includes(priorNoteType);
         note.type = canBecomeSustain ? NoteType.SUSTAIN : NoteType.ATTACK;
         return;
       }
       case NoteType.ATTACK: {
         note.type = NoteType.REST;
         // If the next note was a SUSTAIN, it becomes an ATTACK
-        const nextNote = this.getNote(row, col+1);
+        const nextNote = this.getNote(row, col + 1);
         if (nextNote === undefined || nextNote.type !== NoteType.SUSTAIN)
           return;
         nextNote.type = NoteType.ATTACK;
@@ -144,11 +178,59 @@ export class BarLine {
     }
   }
 
+  /**
+   * Sets the dynamics for the given column
+   *
+   * @param col The column to set the dynamics for
+   * @param dynamic A number from 0-1
+   * @throws if `col` is out of range
+   */
+  setDynamics(col: number, dynamic: number) {
+    if (col < 0 || col >= BarLine.COLS)
+      throw new Error(`Column ${col} is out of range`);
+    this.state.dynamics[col] = dynamic;
+  }
+
+  /**
+   * Gets the dynamics for the given column
+   *
+   * @param col The column to get the dynamics for
+   * @returns A number from 0-1
+   * @throws if `col` is out of range
+   */
+  getDynamics(col: number) {
+    if (col < 0 || col >= BarLine.COLS)
+      throw new Error(`Column ${col} is out of range`);
+    return this.state.dynamics[col];
+  }
+
+  /**
+   * Sets the volume for the entire barline
+   *
+   * @param volume A number from 0-1
+   */
+  setMasterVolume(volume: number) {
+    this.state.masterVolume = volume;
+  }
+
+  /**
+   * To send the BarLine over the network, we need to convert it to a
+   * plain object. This method converts the BarLine into a JOSN object.
+   * To reconstruct the BarLine, use {@link BarLine.fromObject}
+   *
+   * @example
+   * const barLine = new BarLine();
+   * const barLineObject = barLine.toObject();
+   * // barLineObject is now a JSON object that can be sent over the network
+   * // and then converted back to a BarLine using BarLine.fromObject
+   */
   toObject() {
     return _.cloneDeep(this.state);
   }
 
   /**
+   * Constructs a new BarLine instance from the given object.
+   *
    * @example
    * // The object data
    * const barLineObject = { ... };
--- common/src/ensembles/Ensemble.ts
+++ common/src/ensembles/Ensemble.ts
@@ -4,6 +4,7 @@ import _ from "lodash";
 import { v4 as uuidv4 } from "uuid";
 import { Instrument } from "./Note.js";
 
+/** The Eight Bars for a single instrument */
 export type EnsembleObject = {
   /** The ID of the ensemble */
   id: string;
@@ -34,17 +35,36 @@ export class Ensemble {
     console.log(`Made new Ensemble with ID: ${this.state.id}`);
   }
 
-  private getBarLine(userId: UserID) {
+  /**
+   * Returns the BarLine that belongs to `userId`. Returns undefined if the
+   * user does not have a BarLine.
+   *
+   * @param userId The user to get the BarLine for
+   * @returns The user's respective BarLine
+   */
+  getBarLine(userId: UserID) {
     return this.state.arrangement.find(
       (barline) => barline.getAuthor() === userId,
     );
   }
 
+  /**
+   * Adds the user to the Ensemble and gives them a BarLine. The initial
+   * state of the BarLine is determined by the {@link BarLine} constructor.
+   *
+   * @param userId
+   */
   joinRoom(userId: UserID) {
     this.state.authors.push(userId);
     this.state.arrangement.push(new BarLine(userId));
   }
 
+  /**
+   * Removes the user and their BarLine from the Ensemble.
+   * Does not error if the user is not in the Ensemble.
+   *
+   * @param userId The user to remove
+   */
   leaveRoom(userId: UserID) {
     this.state.authors = this.state.authors.filter((e) => e !== userId);
     this.state.arrangement = this.state.arrangement.filter(
@@ -52,29 +72,72 @@ export class Ensemble {
     );
   }
 
+  /**
+   * Sets the instrument of the BarLine that belongs to `userId` to `instrument`.
+   *
+   * Specifications
+   * --------------
+   * - Does not care if another user has the same instrument.
+   * - See {@link BarLine.setInstrument}
+   * @throws if the user does not have a BarLine
+   */
   setInstrument(userId: UserID, instrument: Instrument) {
     const barLine = this.getBarLine(userId);
-    /** @todo handle this error better */
-    if (barLine === undefined) return;
+    if (barLine === undefined)
+      throw new Error(`User ${userId} does not have a BarLine`);
     barLine.setInstrument(instrument);
   }
 
   /**
    * Toggles the note at position (`row`, `col`) in the BarLine that belongs to `userId`.
-   * 
+   *
    * Specifications
    * --------------
    * - See {@link BarLine.toggleNote}
+   * @throws if the user does not have a BarLine
    */
-  toggleNote(userId: UserID, row: number, col: number, direction=NoteToggleDirection.FORWARD) {
-    return this.getBarLine(userId)?.toggleNote(row, col, direction);
+  toggleNote(
+    userId: UserID,
+    row: number,
+    col: number,
+    direction = NoteToggleDirection.FORWARD,
+  ) {
+    const barLine = this.getBarLine(userId);
+    if (barLine === undefined)
+      throw new Error(`User ${userId} does not have a BarLine`);
+    return barLine.toggleNote(row, col, direction);
   }
 
-  /** Returns a copy of the list of userIDs */
+  /**
+   * Returns the note at position (`row`, `col`) in the BarLine that belongs to `userId`.
+   *
+   * @throws if the user does not have a BarLine
+   */
+  getNote(userId: UserID, row: number, col: number) {
+    const barLine = this.getBarLine(userId);
+    if (barLine === undefined)
+      throw new Error(`User ${userId} does not have a BarLine`);
+    return barLine.getNote(row, col);
+  }
+
+  /**
+   * Returns an immutable copy of the list of userIDs
+   */
   getMembers(): readonly string[] {
     return Array.from(this.state.authors);
   }
 
+  /**
+   * To send the Ensemble between the server and client, we need to convert
+   * it to a JSON object. This method converts the Ensemble into a JSON object.
+   * To reconstruct the Ensemble, use {@link Ensemble.fromObject}
+   *
+   * @example
+   * const ensemble = new Ensemble();
+   * const ensembleObject = ensemble.toObject();
+   * // ensembleObject is now a JSON object that can be sent over the network
+   * // and then converted back to an Ensemble using Ensemble.fromObject
+   */
   toObject(): EnsembleObject {
     const clone = _.cloneDeep(this.state);
     const nestedClone: EnsembleObject = {
@@ -85,7 +148,8 @@ export class Ensemble {
   }
 
   /**
-   * Creates the `Ensemble` class from the `EnsembleObject` representation
+   * Creates the `Ensemble` class from the `EnsembleObject` representation. To
+   * generate an `EnsembleObject` from an `Ensemble`, use {@link Ensemble.toObject}
    *
    * @example
    * const ensemble = Ensemble.fromObject({
