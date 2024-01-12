5bc4877b8dd1cfcf9bc304ad30db29915bcfc4d4
Tom <tommy.seng.heng@gmail.com>
2024-01-12 01:38:39 -0500

    fix

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -20,7 +20,7 @@ const __dirname = path.dirname(__filename);
 
 const PORT = process.env.PORT || DEFAULT_SERVER_PORT;
 
-export const app = express();
+const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
