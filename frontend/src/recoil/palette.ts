4269fe8fae7ee0d36c2c56d54a1bb703e72c37e4
JoshWannaPaas <paasjoshua@yahoo.com>
2024-01-11 21:24:47 -0800

    feat(frontend): added selected palette to local storage

--- frontend/src/recoil/palette.ts
+++ frontend/src/recoil/palette.ts
@@ -1,7 +1,27 @@
-import { atom } from "recoil";
+import { AtomEffect, atom } from "recoil";
 import { Palette } from "../ui-components/Palette";
+
+const store = typeof window !== "undefined" ? window.localStorage : null;
+
+export const localStoragePaletteEffect: (key: string) => AtomEffect<Palette> =
+  (key) =>
+  ({ setSelf, onSet }) => {
+    if (store) {
+      const savedValue = store.getItem(key);
+      if (savedValue != null) {
+        setSelf(JSON.parse(savedValue));
+      }
+
+      onSet((newValue, _, isReset) => {
+        isReset
+          ? store.removeItem(key)
+          : store.setItem(key, JSON.stringify(newValue));
+      });
+    }
+  };
 
 export const paletteAtom = atom<Palette>({
   key: "paletteAtom",
   default: Palette.GRAYSCALE,
+  effects: [localStoragePaletteEffect("current_user")],
 });
