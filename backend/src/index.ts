da4ba9edf2fc215b9765c6b2c369e71caef6e936
Tom <tommy.seng.heng@gmail.com>
2024-01-12 01:15:10 -0500

    feat(backend): placeholder REST API for users

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,16 +13,31 @@ import registerRoomEvents from "./routes/rooms.js";
 import path from "path";
 import { fileURLToPath } from "url";
 import registerEnsembleEvents from "./routes/ensemble.js";
+import usersRouter from "./routes/users.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const PORT = process.env.PORT || DEFAULT_SERVER_PORT;
 
-const app = express();
+export const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// --- Sets up the REST API endpoints for CRUD operations
+
+app.use("/api/users", usersRouter);
+
+// --- Sets up the static files for the compiled React
+
 app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+// Serve the compiled React code for all other routes
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+});
+
+// --- Sets up the `socket.io` server for RTC
 
 const httpServer = createServer(app);
 
@@ -49,11 +64,6 @@ io.on("connection", (socket) => {
 
   registerRoomEvents(io, socket);
   registerEnsembleEvents(io, socket);
-});
-
-// Serve the index.html file for all routes
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
 });
 
 httpServer.listen(PORT, () => {
