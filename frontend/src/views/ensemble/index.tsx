3355277512c4be1a2805d1241b4c9a2966dae8f5
JoshWannaPaas <paasjoshua@yahoo.com>
2024-01-15 23:13:42 -0800

    feat: ensemble communication between clients and server

--- frontend/src/views/ensemble/index.tsx
+++ frontend/src/views/ensemble/index.tsx
@@ -14,10 +14,10 @@ import {
 import BarLineVisualizer from "./barline/BarLineVisualizer";
 import { useParams } from "react-router-dom";
 import Metronome from "./Metronome";
-import { useRecoilState, useRecoilValue } from "recoil";
+import { useRecoilState, useRecoilValue, useRecoilValueLoadable } from "recoil";
 import { currentInstrumentAtom } from "../../recoil/instrument";
 import { Instrument } from "common/dist/ensembles/Note";
-import { userIDSelector } from "../../recoil/socket";
+import { socketAtom, userIDSelector } from "../../recoil/socket";
 import { ensembleAtom } from "../../recoil/ensemble";
 
 const EnsembleView: FC = () => {
@@ -29,10 +29,23 @@ const EnsembleView: FC = () => {
     setInstrument(e.target.value);
   };
 
+  const { state, contents: socket } = useRecoilValueLoadable(socketAtom);
   const userID = useRecoilValue(userIDSelector);
   const testID = "aaa";
 
   const currentEnsemble = useRecoilValue(ensembleAtom);
+
+  useEffect(() => {
+    // I need to check if we have the value before we continue to do anything
+    if (state !== "hasValue") return undefined;
+    
+    const serverEnsembleUpdateHandler = () => {
+      console.log("Ensemble Update Received from Server")
+      
+    };
+    socket.on("ensemble:update", serverEnsembleUpdateHandler);
+  }, [socket, state, currentEnsemble]);
+
   useEffect(() => {
     currentEnsemble.joinRoom(userID);
     return () => currentEnsemble.leaveRoom(userID);
@@ -98,6 +111,26 @@ const EnsembleView: FC = () => {
         <br />
         <BarLineVisualizer author={testID} />
       </Container>
+      <br />
+      <br />
+      <br />
+      {/* Other play area */}
+      <Container
+        sx={{
+          margin: "auto",
+          marginTop: "2%",
+          alignSelf: "center",
+          alignItems: "center",
+          padding: "0px",
+        }}
+      >
+        <Barline />
+        <br />
+        <VolumeRow />
+      </Container>
+      <br />
+      <br />
+      <br />
     </main>
   );
 };
