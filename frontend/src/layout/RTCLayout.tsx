b052ed2b0e15fa9bd77d0b757e4f259e354ece3c
JoshWannaPaas <paasjoshua@yahoo.com>
2024-01-25 15:06:28 -0800

    chore

--- frontend/src/layout/RTCLayout.tsx
+++ frontend/src/layout/RTCLayout.tsx
@@ -5,6 +5,18 @@ import { ensembleAtom, userListAtom } from "../recoil/ensemble";
 import { socketAtom } from "../recoil/socket";
 import { Ensemble, EnsembleObject } from "common/dist";
 
+/**
+ * This is a separate layout for Real Time Collaboration (RTC)
+ *
+ * When navigating to the Ensemble view, this component gets loaded.
+ * When this happens, we communicate with the server to set up the necessary parts
+ * for RTC, using userListAtom and ensembleAtom, before it gets used.
+ *
+ * We do this to be ready to "catch" the information given to us by the server when we join an Ensemble.
+ * Otherwise, the information is sent too quickly for us to receive, store, and utilize the data.
+ *
+ * @returns
+ */
 const RTCLayout: React.FC = () => {
   // Attach ALL passive event listeners
   const { state, contents: socket } = useRecoilValueLoadable(socketAtom);
