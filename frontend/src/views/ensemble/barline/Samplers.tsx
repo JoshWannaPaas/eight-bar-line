50da008630c322d9c51c1b6e72ed119142543db4
Tommy Heng <tommy.seng.heng@gmail.com>
2024-02-01 22:39:44 -0500

    refactor(frontend): make a dict of samplers

--- frontend/src/views/ensemble/barline/Samplers.tsx
+++ frontend/src/views/ensemble/barline/Samplers.tsx
@@ -5,6 +5,7 @@ import GuitarB2 from "frontend/src/assets/samples/GuitarB2.wav";
 import BassF2 from "frontend/src/assets/samples/BassF2.wav";
 import AltoSaxDb4 from "frontend/src/assets/samples/AltoSaxDb4.wav";
 import TubaD3 from "frontend/src/assets/samples/TubaD3.wav";
+import { Instrument } from "common/dist";
 
 export const fluteSampler = new Tone.Sampler({
   urls: {
@@ -47,3 +48,19 @@ export const tubaSampler = new Tone.Sampler({
   },
   release: 1,
 }).toDestination();
+
+/**
+ * The Tone.js samplers for each instrument
+ *
+ * @note The Record type enforces that all Instrument enums are covered.
+ * If we add a new instrument, this will throw a type error until we add
+ * the new instrument to the Record.
+ */
+export const samplers: Record<Instrument, Tone.Sampler> = {
+  [Instrument.FLUTE]: fluteSampler,
+  [Instrument.MARIMBA]: marimbaSampler,
+  [Instrument.GUITAR]: guitarSampler,
+  [Instrument.BASS]: bassSampler,
+  [Instrument.ALTO_SAX]: altoSaxSampler,
+  [Instrument.TUBA]: tubaSampler,
+};
