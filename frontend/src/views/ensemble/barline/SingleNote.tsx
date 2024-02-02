a39cfc9cb6c4abe42eebabb036dc897d6e4f98e1
Tommy Heng <tommy.seng.heng@gmail.com>
2024-02-01 22:00:44 -0500

    temp

--- frontend/src/views/ensemble/barline/SingleNote.tsx
+++ frontend/src/views/ensemble/barline/SingleNote.tsx
@@ -1,7 +1,7 @@
 import { Instrument, NoteType, UserID } from "common/dist";
 import { FC, useEffect, useState } from "react";
 import { Box } from "@mui/material";
-import { useRecoilState, useRecoilValue, useRecoilValueLoadable } from "recoil";
+import { useRecoilValue, useRecoilValueLoadable } from "recoil";
 import { beatNumberAtom } from "../../../recoil/beat";
 import { paletteAtom } from "../../../recoil/palette";
 import { paletteDict } from "../../../ui-components/Palette";
@@ -26,10 +26,24 @@ interface SingleNoteProps {
 
 const PITCH_VALUES = ["C5", "B4", "A4", "G4", "F4", "E4", "D4", "C4"];
 
+/**
+ * A single square on the main board of the Ensemble View
+ * 
+ * Specifications
+ * --------------
+ * - Highlights when the user hovers over it only when the user is the author
+ * - Plays the note's sound when the beat number is the current beat number
+ * - When clicked, sends a socket event to toggle the note. The server will 
+ *   trigger an update for all clients.
+ */
 const SingleNote: FC<SingleNoteProps> = ({ beatNumber, pitch, author }) => {
+  /** The current color palette, for rendering the correct colors */
   const palette = useRecoilValue(paletteAtom);
-  const [currentEnsemble, setCurrentEnsemble] = useRecoilState(ensembleAtom);
+  /** The current state of the ensemble */
+  const currentEnsemble = useRecoilValue(ensembleAtom);
+  /** Our socket connection, for sending the socket events */
   const { state, contents: socket } = useRecoilValueLoadable(socketAtom);
+  /** Our user ID, for checking if we are the author */
   const userID = useRecoilValue(userIDSelector);
 
   // Colors for notes
@@ -46,55 +60,28 @@ const SingleNote: FC<SingleNoteProps> = ({ beatNumber, pitch, author }) => {
     beatNumber,
   ).type;
 
-  const globalBeatNumber = useRecoilValue(beatNumberAtom);
+  // Load the sampler for the current instrument
   const currentInstrument = currentEnsemble.getInstrument(author);
+  const sampler = getSampler(currentInstrument);
+  
+  // Load the global beat number and check if we need to play
+  const globalBeatNumber = useRecoilValue(beatNumberAtom);
   const playNow = globalBeatNumber === beatNumber;
-  const now = Tone.now();
 
   useEffect(() => {
     // Do nothing if music is not being played
     if (!playNow) return;
 
-    // Assign correct instrument sounds to what player selected
-    const sampler = getSampler(currentInstrument);
-
     // Trigger a music note if we are not a NoteType.REST
     if (currentNoteType === NoteType.ATTACK) {
-      Tone.Transport.scheduleOnce((time) => {
-        sampler.triggerAttackRelease(PITCH_VALUES[pitch], "4n", time);
-      }, Tone.now());
+      console.log("Playing Now");
+      sampler.triggerAttackRelease(PITCH_VALUES[pitch], "4n");
+      // Tone.Transport.scheduleOnce((time) => {
+      //   sampler.triggerAttackRelease(PITCH_VALUES[pitch], "4n", time);
+      // }, 0);
     }
-  }, [
-    playNow,
-    pitch,
-    currentNoteType,
-    currentInstrument,
-    now,
-    beatNumber,
-    currentEnsemble,
-    userID,
-    author,
-  ]);
+  }, [playNow, pitch, currentNoteType, currentInstrument, beatNumber, currentEnsemble, userID, author, sampler]);
 
-  // Change instrument sound based on currently selected
-  const getSampler = (instrument: Instrument) => {
-    switch (instrument) {
-      case Instrument.FLUTE:
-        return fluteSampler;
-      case Instrument.ALTO_SAX:
-        return altoSaxSampler;
-      case Instrument.MARIMBA:
-        return marimbaSampler;
-      case Instrument.GUITAR:
-        return guitarSampler;
-      case Instrument.BASS:
-        return bassSampler;
-      case Instrument.TUBA:
-        return tubaSampler;
-      default:
-        return fluteSampler;
-    }
-  };
 
   // Store if we are currently hovering over it
   const [onHover, setOnHover] = useState(false);
@@ -112,23 +99,16 @@ const SingleNote: FC<SingleNoteProps> = ({ beatNumber, pitch, author }) => {
   // When click, update current note type
   const handleClick = () => {
     if (state !== "hasValue") return;
-    if (userID === author) {
-      // Local update
-      currentEnsemble.toggleNote(userID, pitch, beatNumber);
-      setCurrentEnsemble(currentEnsemble);
+    if (userID === author) 
       socket.emit("ensemble:toggle-note", pitch, beatNumber);
-    }
   };
 
   // Color depends on Hover and NoteType
-  let noteColor = colorMapping[NoteType.REST];
   let highlight = 1; // 100% Brightness which is Normal Brightness
 
   // Coloring
-  if (currentNoteType === NoteType.ATTACK)
-    noteColor = colorMapping[NoteType.ATTACK];
-  if (currentNoteType === NoteType.SUSTAIN)
-    noteColor = colorMapping[NoteType.SUSTAIN];
+  const noteColor = colorMapping[currentNoteType];
+
   if (onHover || playNow) {
     if (currentNoteType === NoteType.REST) highlight = 1.1;
     if (currentNoteType === NoteType.SUSTAIN) highlight = 1.4;
@@ -151,3 +131,23 @@ const SingleNote: FC<SingleNoteProps> = ({ beatNumber, pitch, author }) => {
   );
 };
 export default SingleNote;
+
+// Change instrument sound based on currently selected
+const getSampler = (instrument: Instrument) => {
+  switch (instrument) {
+    case Instrument.FLUTE:
+      return fluteSampler;
+    case Instrument.ALTO_SAX:
+      return altoSaxSampler;
+    case Instrument.MARIMBA:
+      return marimbaSampler;
+    case Instrument.GUITAR:
+      return guitarSampler;
+    case Instrument.BASS:
+      return bassSampler;
+    case Instrument.TUBA:
+      return tubaSampler;
+    default:
+      return fluteSampler;
+  }
+};
