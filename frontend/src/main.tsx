9ebff2f94988765f2c4bfa0cdafc54d051bfe13c
Joshua Paas <paasjoshua@yahoo.com>
2024-02-08 19:11:27 -0800

    feat(full): leave room (#61)

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,12 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <RecoilRoot>
-      <Router>
-        <App />
-      </Router>
-    </RecoilRoot>
-  </React.StrictMode>,
+  <RecoilRoot>
+    <Router>
+      <App />
+    </Router>
+  </RecoilRoot>,
 );
