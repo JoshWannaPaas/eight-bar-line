228c6f76af976817be35e202ef2bea9a27ca2219
JoshWannaPaas <paasjoshua@yahoo.com>
2024-02-28 15:41:08 -0800

    drawer navigation

--- frontend/src/layout/MainLayout.tsx
+++ frontend/src/layout/MainLayout.tsx
@@ -1,23 +1,47 @@
 import {
   Box,
+  Button,
   Container,
   CssBaseline,
+  Drawer,
   FormControl,
   InputLabel,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
   MenuItem,
   Paper,
   Select,
   SelectChangeEvent,
   styled,
 } from "@mui/material";
-import { FC } from "react";
-import { Outlet, Link } from "react-router-dom";
+import { FC, useState } from "react";
+import { Outlet, Link, useNavigate, redirect } from "react-router-dom";
 import { paletteAtom } from "../recoil/palette";
 import { paletteDict, Palette } from "../ui-components/Palette";
 import { useRecoilState, useRecoilValue } from "recoil";
 import ErrorPage from "../views/ErrorPage";
 import ErrorBoundary from "../ui-components/ErrorBoundary";
 import { currentLoginAtom } from "../recoil/login";
+import drawerImage from "../assets/drawers/drawer grayscale alt.png";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import HomeIcon from "@mui/icons-material/Home";
+import GroupsIcon from "@mui/icons-material/Groups";
+import CreateIcon from "@mui/icons-material/Create";
+import ClearIcon from "@mui/icons-material/Clear";
+import SearchIcon from "@mui/icons-material/Search";
+
+const drawerItems = ["Home", "Ensemble", "Create", "Browse", "Account"];
+const drawerIcons = [
+  <HomeIcon />,
+  <GroupsIcon />,
+  <CreateIcon />,
+  <SearchIcon />,
+  <AccountBoxIcon />,
+];
+const drawerNavigation = ["/", "/ensemble", "/create", "/browse", "/login"];
 
 const MainLayout: FC = () => {
   const [palette, setPalette] = useRecoilState(paletteAtom);
@@ -27,6 +51,88 @@ const MainLayout: FC = () => {
   };
   const currentUser = useRecoilValue(currentLoginAtom);
 
+  const [isOpen, setOpen] = useState(false);
+  const toggleDrawer = (newOpen: boolean) => () => {
+    setOpen(newOpen);
+  };
+
+  const onRedirect = (page: string) => {
+    return redirect(page);
+  };
+
+  const DrawerItems = (
+    <Box sx={{ width: "300px", paddingTop: "10px" }}>
+      {/* Drawer Navigation */}
+      <List>
+        <ListItem key="Close" disablePadding>
+          <ListItemButton onClick={toggleDrawer(false)}>
+            <ListItemIcon>
+              <ClearIcon />
+            </ListItemIcon>
+            <ListItemText
+              primary="Close"
+              primaryTypographyProps={{
+                fontSize: "24px",
+                fontFamily: "Segoe UI",
+              }}
+            />
+          </ListItemButton>
+        </ListItem>
+        {drawerItems.map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <Link to={drawerNavigation[index]}>
+              <ListItemButton sx={{ width: "200%" }}>
+                <ListItemIcon>{drawerIcons[index]}</ListItemIcon>
+                <ListItemText
+                  primary={text}
+                  primaryTypographyProps={{
+                    fontSize: "24px",
+                    fontFamily: "Segoe UI",
+                  }}
+                />
+              </ListItemButton>
+            </Link>
+          </ListItem>
+        ))}
+
+        {/* Palette Control */}
+        <ListItem>
+          <FormControl
+            fullWidth
+            variant="filled"
+            sx={{ backgroundColor: "white", borderRadius: "5px" }}
+          >
+            <InputLabel>Palette</InputLabel>
+            <Select
+              value={palette}
+              label="palette"
+              onChange={changePaletteHandler}
+              fullWidth
+              size="small"
+            >
+              {/* Needs refactoring for ease of expansion */}
+              <MenuItem value={Palette.GRAYSCALE}>
+                {paletteDict[Palette.GRAYSCALE].name}
+              </MenuItem>
+              <MenuItem value={Palette.SUNSET}>
+                {paletteDict[Palette.SUNSET].name}
+              </MenuItem>
+              <MenuItem value={Palette.MARINE}>
+                {paletteDict[Palette.MARINE].name}
+              </MenuItem>
+              <MenuItem value={Palette.GOLDENROD}>
+                {paletteDict[Palette.GOLDENROD].name}
+              </MenuItem>
+              <MenuItem value={Palette.FOREST}>
+                {paletteDict[Palette.FOREST].name}
+              </MenuItem>
+            </Select>
+          </FormControl>
+        </ListItem>
+      </List>
+    </Box>
+  );
+
   return (
     <Box>
       <CssBaseline />
@@ -34,85 +140,30 @@ const MainLayout: FC = () => {
         sx={{
           width: "100%",
           bgcolor: paletteDict[palette].sustain,
-          paddingTop: "10px",
-          paddingBottom: "10px",
+          paddingLeft: "50px",
           borderRadius: "0px 0px 5px 5px",
+          display: "flex",
+          gap: 1,
         }}
       >
-        <Container sx={{ display: "flex", gap: 2 }}>
-          <Box flex={1}>
-            <TitleButtons>
-              <Link to={"/"}>8Bar Line</Link>
-            </TitleButtons>
-          </Box>
-          <Box flex={1} />
-          <Box flex={1}>
-            <TitleButtons>
-              <Link to={"/create"}>Create</Link>
-            </TitleButtons>
-          </Box>
-          <Box flex={1}>
-            <TitleButtons>
-              <Link to={"/ensemble"}>Ensemble</Link>
-            </TitleButtons>
-          </Box>
-          <Box flex={1}>
-            <TitleButtons>
-              <Link to={"/browse"}>Browse</Link>
-            </TitleButtons>
-          </Box>
-          <Box flex={1}>
-            <TitleButtons>
-              <Link to={"/login"}>Login</Link>
-            </TitleButtons>
-          </Box>
-          <Box flex={1} sx={{ width: "50%" }}>
-            <FormControl
-              fullWidth
-              variant="filled"
-              sx={{ backgroundColor: "white", borderRadius: "5px" }}
-            >
-              <InputLabel>Palette</InputLabel>
-              <Select
-                value={palette}
-                label="palette"
-                onChange={changePaletteHandler}
-                fullWidth
-                size="small"
-              >
-                <MenuItem value={Palette.GRAYSCALE}>
-                  {paletteDict[Palette.GRAYSCALE].name}
-                </MenuItem>
-                <MenuItem value={Palette.SUNSET}>
-                  {paletteDict[Palette.SUNSET].name}
-                </MenuItem>
-                <MenuItem value={Palette.MARINE}>
-                  {paletteDict[Palette.MARINE].name}
-                </MenuItem>
-                <MenuItem value={Palette.GOLDENROD}>
-                  {paletteDict[Palette.GOLDENROD].name}
-                </MenuItem>
-                <MenuItem value={Palette.FOREST}>
-                  {paletteDict[Palette.FOREST].name}
-                </MenuItem>
-              </Select>
-            </FormControl>
-          </Box>
-          <Box flex={1}>
-            <TitleButtons>Hello, {currentUser.username}!</TitleButtons>
-          </Box>
-        </Container>
+        <Button onClick={toggleDrawer(true)} sx={{ width: "5%" }}>
+          <img src={drawerImage} style={{ imageRendering: "pixelated" }} />
+        </Button>
+        <h1>8Bar Line</h1>
       </Box>
+
+      <Drawer
+        open={isOpen}
+        onClose={toggleDrawer(false)}
+        PaperProps={{ sx: { backgroundColor: paletteDict[palette].rest } }}
+      >
+        {DrawerItems}
+      </Drawer>
       <ErrorBoundary errorPage={ErrorPage}>
         <Outlet />
       </ErrorBoundary>
     </Box>
   );
 };
-
-const TitleButtons = styled(Paper)(({ theme }) => ({
-  padding: theme.spacing(1),
-  textAlign: "center",
-}));
 
 export default MainLayout;
